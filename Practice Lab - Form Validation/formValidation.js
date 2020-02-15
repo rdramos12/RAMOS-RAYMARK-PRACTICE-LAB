08425e9b58725a3c9e13b875947dea0d5b677f0b
rdramos12 <60497239+rdramos12@users.noreply.github.com>
2020-02-15 14:06:35 +0800

    PRATCICE LAB - FORM VALIDATION
    RAMOS, Raymark D.
    2ITE

---
+++ Practice Lab - Form Validation/formValidation.js
@@ -0,0 +1,14 @@
+function validate() {
+    let event_type1 = document.getElementById("event_type").value;
+    let event_title1 = document.getElementById("event_title").value;
+    let date_begin1 = document.getElementById("date_begin").value;
+    let date_end1 = document.getElementById("date_end").value;
+    let date_posted1 = document.getElementById("date_posted").value;
+    let reg_require1 = document.getElementById("reg_require").value;
+    let reg_fam_prog1 = document.getElementById("reg_fam_prog").value;
+    let reg_email_rec1 = document.getElementById("reg_email_rec").value;
+    let point_cont1 = document.getElementById("point_cont").value;
+    let host_cont1 = document.getElementById("host_cont").value;
+
+    alert("FORM VALIDATED");
+}
